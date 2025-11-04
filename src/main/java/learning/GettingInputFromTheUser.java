dcfa2a42f579add271d202da8016b9c17c0317e9
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-04 09:19:24 -0500

    basic calculator1 loop added

--- src/main/java/learning/GettingInputFromTheUser.java
+++ src/main/java/learning/GettingInputFromTheUser.java
@@ -6,40 +6,49 @@ public class GettingInputFromTheUser {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String again = "yes";
+        String breakProgram = "no";
 
-        System.out.print("Enter first number:");
-        double num1 = scanner.nextDouble();
+        while (again.equalsIgnoreCase("yes")) {
 
-        System.out.print("Enter second number:");
-        double num2 = scanner.nextDouble();
+            System.out.print("Enter first number:");
+            double num1 = scanner.nextDouble();
 
-        System.out.println("What operation would you like to perform?:");
-        System.out.println("1) for addition");
-        System.out.println("2) for subtraction");
-        System.out.println("3) for multiplication");
-        System.out.println("4) for division");
-        System.out.print("Choose one:");
-        int choice = scanner.nextInt();
+            System.out.print("Enter second number:");
+            double num2 = scanner.nextDouble();
 
-        switch (choice) {
-            case 1:
-                System.out.println("Result:"+ ( num1 + num2));
-                break;
-            case 2:
-                System.out.println("Result:" +( num1 - num2));
-                break;
-            case 3:
-                System.out.println("Result:"+ (num1 * num2));
-                break;
-            case 4:
-                if (num2 == 0) {
-                    System.out.println("Error: Cannot divide by zero!");
-                } else {
-                    System.out.println("Result:"+ (num1 / num2));
-                }
-                break;
+            System.out.println("What operation would you like to perform?:");
+            System.out.println("1) for addition");
+            System.out.println("2) for subtraction");
+            System.out.println("3) for multiplication");
+            System.out.println("4) for division");
+            System.out.print("Choose one:");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result:" + (num1 + num2));
+                    break;
+                case 2:
+                    System.out.println("Result:" + (num1 - num2));
+                    break;
+                case 3:
+                    System.out.println("Result:" + (num1 * num2));
+                    break;
+                case 4:
+                    if (num2 == 0) {
+                        System.out.println("Error: Cannot divide by zero!");
+                    } else {
+                        System.out.println("Result:" + (num1 / num2));
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+            System.out.println("Would you like to perform another calculation? (yes/no)");
+            again = scanner.next();
+
         }
         scanner.close();
-
     }
 }
