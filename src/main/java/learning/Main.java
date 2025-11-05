303da33132859bfa01eb28b2e65b4fb2d3befb04
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-05 10:01:47 -0500

    class(comparing persons)

--- src/main/java/learning/Main.java
+++ src/main/java/learning/Main.java
@@ -2,12 +2,22 @@ package learning;
 
 public class Main {
     public static void main(String[] args) {
-         Person person = new Person();
+         Person person1 = new Person();
+        Person person2 = new Person();
 
-         person.name = "Vincent";
-         person.age = 23;
-         person.height = 179;
+         person1.name = "Vincent";
+         person1.age = 23;
+         person1.height = 179;
 
-       person.introduce();
+         person2.name= "Alice";
+         person2.age = 32;
+         person2.height =167;
+
+       person1.introduce();
+       person2.introduce();
+
+       person1.compareAge(person2);
+
+
     }
 }
--- src/main/java/learning/Person.java
+++ src/main/java/learning/Person.java
@@ -9,4 +9,21 @@ public class Person {
         System.out.println("Hi, my name is " + name + ".");
         System.out.println("I am " + age + " years old and " + height + " cm tall.");
     }
+
+    void birthday() {
+        age += 1;
+        System.out.println("Happy birthday, " + name + "! You are now " + age + " years old.");
+    }
+
+    void compareAge(Person otherPerson) {
+        if (this.age > otherPerson.age) {
+            System.out.println(name + " is older than " + otherPerson.name + ".");
+        } else if (this.age < otherPerson.age) {
+            System.out.println(name + " is younger than " + otherPerson.name + ".");
+        } else {
+            System.out.println(name + " and " + otherPerson.name + " are the same age!");
+        }
+    }
 }
+
+
