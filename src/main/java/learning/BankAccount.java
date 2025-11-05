4e8ce0b051b776f7a7652da6920634b3b79ac22c
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-05 14:09:35 -0500

    bank account class

---
+++ src/main/java/learning/BankAccount.java
@@ -0,0 +1,39 @@
+package learning;
+
+public class BankAccount {
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountHolderName, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Deposit amount must be positive.");
+        } else {
+            balance += amount; // add money to the balance
+            System.out.println("Successfully deposited $" + amount + ". New balance: $" + balance);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdrawal amount must be positive.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient funds. Current balance: $" + balance);
+        } else {
+            balance -= amount; // subtract the amount from the balance
+            System.out.println("Successfully withdrew $" + amount + ". New balance: $" + balance);
+        }
+    }
+}
