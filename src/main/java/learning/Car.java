15fe4daaadec7bbbf43c6041a9cd4c9217dbd500
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-05 13:46:13 -0500

    car class

---
+++ src/main/java/learning/Car.java
@@ -0,0 +1,17 @@
+package learning;
+
+public class Car {
+    String make;
+    String model;
+    int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    void displayInfo() {
+        System.out.println("This car is a " +year +" "+make +" "+ model);
+    }
+}
