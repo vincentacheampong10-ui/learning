86397699a3cc3a056afde6b9c0c85bf20baee381
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-05 14:18:18 -0500

    pet class

---
+++ src/main/java/learning/Pet.java
@@ -0,0 +1,43 @@
+package learning;
+
+public class Pet {
+    private String name;
+    private String type; // e.g. "Dog", "Cat", "Parrot"
+    private int age;
+
+    public Pet(String name, String type, int age) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Invalid age. Setting age to 0.");
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+}
