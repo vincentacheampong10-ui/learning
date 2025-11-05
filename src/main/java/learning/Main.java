169b9dc8ccd462a29a41b2b484d781b55c3a8fd4
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-05 09:52:42 -0500

    class(Person)

---
+++ src/main/java/learning/Main.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class Main {
+    public static void main(String[] args) {
+         Person person = new Person();
+
+         person.name = "Vincent";
+         person.age = 23;
+         person.height = 179;
+
+       person.introduce();
+    }
+}
---
+++ src/main/java/learning/Person.java
@@ -0,0 +1,12 @@
+package learning;
+
+public class Person {
+    String name;
+    int age;
+    double height;
+
+    void introduce() {
+        System.out.println("Hi, my name is " + name + ".");
+        System.out.println("I am " + age + " years old and " + height + " cm tall.");
+    }
+}
